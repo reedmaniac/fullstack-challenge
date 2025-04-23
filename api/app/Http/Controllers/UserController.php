d90b681a75dff418dbb4fed76b2ecc579e346c67
Paul Burdick <paul@reedmaniac.com>
2025-04-23 14:27:51 -0700

    Tweaked the existing API to specify an /api/ prefix and also one for users.index and users.show.
    And for good measure, I put in paginate and a UserResource to help structure the output in a pretty format

---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Resources\UserDetailResource;
+use App\Http\Resources\UserResource;
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        return UserResource::collection(User::paginate(20));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request, User $user)
+    {
+         return response()->json(
+            new UserDetailResource($user)
+        );
+    }
+}
---
+++ api/app/Http/Resources/UserResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+
+            'id' => (integer) $this->id,
+            'name' => (string) $this->name,
+            'email' => (string) $this->email,
+            'email_verified_at' => $this->email_verified_at->toIso8601String(),
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'created_at' => $this->created_at->toIso8601String(),
+            'updated_at' => $this->updated_at->toIso8601String(),
+        ];
+    }
+}
